b12ad41f1e8c1d67695edafddbbcd13353331960
Stephan Wagner <stephanwagner.me@gmail.com>
2026-03-04 23:46:44 +1000

    Remove activity

--- themes/fromscratch/inc/dashboard.php
+++ themes/fromscratch/inc/dashboard.php
@@ -11,8 +11,11 @@ defined('ABSPATH') || exit;
 //  - Disable in settings
 
 /**
- * Remove WordPress Events and News
+ * Remove WordPress Events and News, and Activity (includes Recent comments).
  */
 add_action('wp_dashboard_setup', function () {
 	remove_meta_box('dashboard_primary', 'dashboard', 'side');
+
+	// TODO Maybe allow?
+	// remove_meta_box('dashboard_activity', 'dashboard', 'normal');
 }, 999);
