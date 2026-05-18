887548e69f6ff0c10f583c437ab6fc11a1e04391
Stephan Wagner <stephanwagner.me@gmail.com>
2026-05-18 22:04:28 +1000

    Remove hr styles

--- theme/fromscratch/src/js/editor/blocks.js
+++ theme/fromscratch/src/js/editor/blocks.js
@@ -1,3 +1,9 @@
 wp.domReady(() => {
+  // Image
   wp.blocks.unregisterBlockStyle('core/image', 'rounded');
+
+  // Separator
+  wp.blocks.unregisterBlockStyle('core/separator', 'default');
+  wp.blocks.unregisterBlockStyle('core/separator', 'wide');
+  wp.blocks.unregisterBlockStyle('core/separator', 'dots');
 });
