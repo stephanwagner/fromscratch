39ea88634a26ea479ed22467b53b128c8e28074c
Stephan Wagner <stephanwagner.me@gmail.com>
2026-05-18 22:50:42 +1000

    Minor formatting

--- theme/fromscratch/templates/read-more.php
+++ theme/fromscratch/templates/read-more.php
@@ -4,7 +4,7 @@ defined('ABSPATH') || exit;
 
 $url = isset($url) ? (string) $url : '';
 $label = isset($label) && is_string($label) ? $label : __('Read more', 'fromscratch');
-$class = isset($class) && is_string($class) ? $class : 'archive__read-more';
+$class = isset($class) && is_string($class) ? $class : '';
 $link_tag = isset($link_tag) && in_array($link_tag, ['a', 'button', 'div', 'span']) ? $link_tag : (!empty($url) ? 'a' : 'span');
 ?>
 <<?= $link_tag ?>
