a1eb4a77627b2de058291bafec6563ed0a78ebf8
Stephan Wagner <stephanwagner.me@gmail.com>
2026-05-24 21:55:19 +1000

    Update no posts found

--- theme/fromscratch/acf/blocks/article-list/article-list.php
+++ theme/fromscratch/acf/blocks/article-list/article-list.php
@@ -8,7 +8,7 @@ $classNames[] = $block['id'];
 
 // Add class provided via class_field in WP Backend
 if (!empty($block['className'])) {
-	$classNames[] = $block['className'];
+    $classNames[] = $block['className'];
 }
 
 // Add wrapper class
@@ -26,6 +26,7 @@ $limitType = get_field('limit-type');
 $limit = get_field('limit');
 $sortBy = get_field('sort-by');
 $sortDirection = get_field('sort-direction');
+$design = get_field('design'); // TODO
 
 // CPT Config
 $cpt = fs_config_cpt($postType);
@@ -34,46 +35,50 @@ $cpt = fs_config_cpt($postType);
 $postsPerPage = -1;
 $paged = 1;
 if ($hasLimit && $limitType === 'pagination') {
-	$postsPerPage = (int) $limit;
-	$paged = max(1, (int) get_query_var('paged'), (int) get_query_var('page'));
+    $postsPerPage = (int) $limit;
+    $paged = max(1, (int) get_query_var('paged'), (int) get_query_var('page'));
 }
 
 // Get articles
 $query = new WP_Query([
-	'post_type'      => $postType,
-	'posts_per_page' => $postsPerPage,
-	'orderby'        => $sortBy,
-	'order'          => $sortDirection,
-	'paged'          => $paged,
+    'post_type'      => $postType,
+    'posts_per_page' => $postsPerPage,
+    'orderby'        => $sortBy,
+    'order'          => $sortDirection,
+    'paged'          => $paged,
 ]);
 
 // Posts
 $posts = $query->posts;
 
 // TODO
-// - no articles list message
+// - test none found
 
 ?>
 
 <div class="<?= implode(' ', $classNames) ?>">
-    <div class="article-list__container">
-        <div class="article-list__items archive__list">
+    <?php if (!empty($posts)) { ?>
+        <div class="article-list__container">
+            <div class="article-list__items -design-<?= esc_attr($design) ?>">
+                <?php
+                foreach ($posts as $post) {
+                    $GLOBALS['post'] = $post;
+                    setup_postdata($post);
+                    fs_render_template('article-preview');
+                }
+                wp_reset_postdata();
+                ?>
+            </div>
             <?php
-            foreach ($posts as $post) {
-                $GLOBALS['post'] = $post;
-                setup_postdata($post);
-                fs_render_template('article-preview');
+            if ($hasLimit && $limitType === 'pagination' && $query->max_num_pages > 1) {
+                fs_render_pagination_for_query($query, [
+                    'aria_label' => __('Articles pagination', 'fromscratch'),
+                    'nav_class'  => 'article-list__pagination',
+                ]);
             }
-            wp_reset_postdata();
             ?>
         </div>
-        <?php
-        if ($hasLimit && $limitType === 'pagination' && $query->max_num_pages > 1) {
-            fs_render_pagination_for_query($query, [
-                'aria_label' => __('Articles pagination', 'fromscratch'),
-                'nav_class'  => 'article-list__pagination',
-            ]);
-        }
-        ?>
-    </div>
+    <?php } else { ?>
+        <div class="article-list__empty"><?= __('No posts found.', 'fromscratch') ?></div>
+    <?php } ?>
 </div>
